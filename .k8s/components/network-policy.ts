8cc97a09e4c3f482792502c17d9ef236708d6271
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2021-04-10 01:53:44 +0200

    fix: use kosko-charts/netpol

--- .k8s/components/network-policy.ts
+++ .k8s/components/network-policy.ts
@@ -1,57 +1,9 @@
 import env from "@kosko/env";
-import { NetworkPolicy } from "kubernetes-models/networking.k8s.io/v1/NetworkPolicy";
-
-/**  Basic netpol that allow :
-/*      - communication inside namespace
-/*      - communication from ingress-controller
-/*      - communication from monitoring
-*/
-
-export const create = (namespace: string): NetworkPolicy =>
-  new NetworkPolicy({
-    metadata: { name: "default", namespace },
-    spec: {
-      podSelector: {},
-      policyTypes: ["Ingress"],
-      ingress: [
-        {
-          from: [
-            {
-              namespaceSelector: {
-                matchLabels: { "network-policy/namespace": namespace },
-              },
-            },
-          ],
-        },
-        {
-          from: [
-            {
-              namespaceSelector: {
-                matchLabels: {
-                  "network-policy/source": "ingress-controller",
-                },
-              },
-            },
-          ],
-        },
-        {
-          from: [
-            {
-              namespaceSelector: {
-                matchLabels: {
-                  "network-policy/source": "monitoring",
-                },
-              },
-            },
-          ],
-        },
-      ],
-    },
-  });
+import { create } from "@socialgouv/kosko-charts/components/netpol";
 
 const manifests = [];
 if (env.env === "prod") {
-  const manifest = create("");
+  const manifest = create("recherche-entreprises");
   manifests.push(manifest);
 }
 export default manifests;
