edfd23365ada3f382f17b8b1871c0e28bbb39126
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2021-04-11 00:55:59 +0200

    fix(api): use CORS

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,11 @@
 import Koa from "koa";
+import cors from "@koa/cors";
+
 import { router } from "./routes";
+
 export const app = new Koa();
+
+app.use(cors());
 
 const port = process.env.API_PORT || 3000;
 
@@ -8,4 +13,4 @@ app.use(router.routes());
 app.listen(port);
 
 console.log(`API started on port ${port}`);
-console.log('ES host:', process.env.ELASTICSEARCH_URL)
+console.log("ES host:", process.env.ELASTICSEARCH_URL);
