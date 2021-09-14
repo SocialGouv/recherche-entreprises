d349ba44b39ce20a549efc0cca473728c8f38228
Remi Mélisson <rmelisson@gmail.com>
2021-09-14 14:05:33 +0200

    chore(lint): lint and set test

--- index/src/index.ts
+++ index/src/index.ts
@@ -1,19 +1,25 @@
+import * as csv from "fast-csv";
 import * as fs from "fs";
 import * as path from "path";
-import * as csv from "fast-csv";
+
 import {
-  esClient,
   createIndex,
-  updateAlias,
   deleteOldIndices,
+  esClient,
   getDocsCount,
+  updateAlias,
 } from "./elastic";
 import { Enterprise, mapEnterprise } from "./enterprise";
 
 const ASSEMBLY_FILE =
   process.env.ASSEMBLY_FILE || "../assembly/output/assembly.csv";
 
-async function* manipulate(stream: any) {
+async function* manipulate(
+  stream: csv.CsvParserStream<
+    csv.ParserRow<Enterprise>,
+    csv.ParserRow<Enterprise>
+  >
+) {
   for await (const enterprise of stream) {
     yield mapEnterprise(enterprise);
   }
@@ -26,37 +32,39 @@ const insertEntreprises = async (indexName: string) => {
   const csvStream = csv.parseStream(stream, { headers: true });
 
   return esClient.helpers.bulk({
-    //@ts-expect-error
+    //@ts-expect-error(expected)
     datasource: manipulate(csvStream),
-    refreshOnCompletion: true,
     onDocument: (enterprise: Enterprise) => ({
       index: {
-        _index: indexName,
         _id: `${enterprise.siret}-${enterprise.idcc}`,
+        _index: indexName,
       },
     }),
+    refreshOnCompletion: true,
   });
 };
 
 if (require.main === module) {
   // use elastic alias feature to prevent downtimes
   console.log(`Creating index`);
-  createIndex().then(async (indexName) => {
-    console.log(`Starting indexation...`);
-    return insertEntreprises(indexName)
-      .then(async () => {
-        console.log(`Indexation complete`);
-        // ensure we have some data
-        const docsCount = await getDocsCount(indexName);
-        if (!docsCount) {
-          throw new Error(
-            `No document created in index ${indexName}, skip aliasing`
-          );
-        } else {
-          console.log(`Created ${docsCount} documents in index ${indexName}`);
-        }
-      })
-      .then(() => updateAlias(indexName))
-      .then(() => deleteOldIndices(indexName));
-  });
+  createIndex()
+    .then(async (indexName) => {
+      console.log(`Starting indexation...`);
+      return insertEntreprises(indexName)
+        .then(async () => {
+          console.log(`Indexation complete`);
+          // ensure we have some data
+          const docsCount = await getDocsCount(indexName);
+          if (!docsCount) {
+            throw new Error(
+              `No document created in index ${indexName}, skip aliasing`
+            );
+          } else {
+            console.log(`Created ${docsCount} documents in index ${indexName}`);
+          }
+        })
+        .then(() => updateAlias(indexName))
+        .then(() => deleteOldIndices(indexName));
+    })
+    .catch((err) => console.log("Error " + JSON.stringify(err, null, 2)));
 }
