3fb386fe7fe5425f21245cbff1da0c422ab6a4b4
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2021-10-18 14:20:17 +0200

    gh actions

--- .k8s/components/api.ts
+++ .k8s/components/api.ts
@@ -1,14 +1,17 @@
 import env from "@kosko/env";
 import { create } from "@socialgouv/kosko-charts/components/app";
-import { getHarborImagePath } from "@socialgouv/kosko-charts/utils/getHarborImagePath";
+import { getGithubRegistryImagePath } from "@socialgouv/kosko-charts/utils/getGithubRegistryImagePath";
 
 const manifests = create("recherche-entreprises-api", {
   config: {
     containerPort: 3000,
-    subDomainPrefix: "api-"
+    subDomainPrefix: "api-",
   },
   deployment: {
-    image: getHarborImagePath({ name: "recherche-entreprises-api" })
+    image: getGithubRegistryImagePath({
+      name: "api",
+      project: "recherche-entreprises",
+    }),
   },
   env,
 });
