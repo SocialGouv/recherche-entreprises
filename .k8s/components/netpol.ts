178510e2c0e2c4b2e81d49b350a2484110422eba
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2021-12-06 17:04:15 +0100

    upgrade

--- .k8s/components/netpol.ts
+++ .k8s/components/netpol.ts
@@ -1,9 +1,5 @@
-import environments from "@socialgouv/kosko-charts/environments";
 import { create } from "@socialgouv/kosko-charts/components/netpol";
 
-const getManifests = () => {
-  const ciEnv = environments(process.env);
-  return create(ciEnv.metadata.namespace.name);
-};
+const getManifests = create();
 
 export default getManifests;
