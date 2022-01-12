b607944e54851d8b68f010ffcea7a78764b22f33
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2022-01-12 10:51:45 +0100

    fix: add more debug info in indexing job

--- sqlite.sh
+++ sqlite.sh
@@ -14,7 +14,6 @@ cd "$(dirname "$0")" || exit
 
 echo "-- Download datasets"
 
-
 if command -v apt-get &> /dev/null
 then
   apt-get update -y
@@ -37,6 +36,12 @@ if ! command -v unzip &> /dev/null
 then
     echo "unzip could not be found"
     apt-get install -y unzip
+fi
+# install md5 if not exists
+if ! command -v md5 &> /dev/null
+then
+    echo "md5 could not be found"
+    apt-get install -y md5
 fi
 
 # geo siret par département
@@ -64,6 +69,11 @@ unzip "${DATA_DIR}/StockUniteLegale_utf8.zip" -d "${DATA_DIR}"
 # WEEZ data
 wget --progress=bar:force:noscroll -q --show-progress https://www.data.gouv.fr/fr/datasets/r/bfc3a658-c054-4ecc-ba4b-22f3f5789dc7 -O "${DATA_DIR}/WEEZ.csv"
 
+echo "-- Data files md5 : "
+for file in "$DATA_DIR"/*.csv; do
+   md5 "$file";
+done
+
 echo "-- Import CSV datasets to sqlite"
 
 sqlite3 -echo "${DATA_DIR}/db.sqlite" ".read import.sql"
@@ -71,3 +81,7 @@ sqlite3 -echo "${DATA_DIR}/db.sqlite" ".read import.sql"
 echo "-- Export sqlite data to ${DATA_DIR}/assembly.csv"
 
 sqlite3 -header -csv "${DATA_DIR}/db.sqlite" ".read export.sql" > "${DATA_DIR}/assembly.csv"
+
+echo "-- Total lines in assembly.csv : "
+wc -l "${DATA_DIR}/assembly.csv"
+md5 "${DATA_DIR}/assembly.csv"
